44efacedf1fde5393c34cbef8962912026dc9dde
njts <mayilov4+github.com@gmail.com>
2022-10-01 20:36:57 +0000

    2022/10/01 20:36:57 UTC  auto commit

--- custom-scripts/auto-git.sh
+++ custom-scripts/auto-git.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 start=`date +%s.%N`
-status=$(git diff --name-only)
 cpuload=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
 ramload=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
 
@@ -23,6 +22,7 @@ echo "$(builddate) build time ${buildtime}         cpu load: ${cpuload} ram load
 git add .
 git commit -m "$(builddate) UTC  auto commit"
 git push -u origin main --force
+status=$(git diff --name-only)
 
 echo "${status}" >> tglog.txt
 pstatus=$(cat tglog.txt | awk 'END{print} ')
