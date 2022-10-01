dea2374a858cd1481923c07d685a25a2c5bc8f1f
njts <mayilov4+github.com@gmail.com>
2022-10-01 20:39:33 +0000

    2022/10/01 20:39:33 UTC  auto commit

--- custom-scripts/auto-git.sh
+++ custom-scripts/auto-git.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 start=`date +%s.%N`
+status=$(git diff --name-only)
 cpuload=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
 ramload=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
 
@@ -22,9 +23,7 @@ echo "$(builddate) build time ${buildtime}         cpu load: ${cpuload} ram load
 git add .
 git commit -m "$(builddate) UTC  auto commit"
 git push -u origin main --force
-status=$(git diff --name-only)
 
-echo "${status}"
 
 # Telegram notification
 /var/www/private-sripts/telegram-notf.sh "
