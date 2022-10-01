1da7dbb39e643e5a9785aa4b94020592c1d2c50b
njts <mayilov4+github.com@gmail.com>
2022-10-01 16:45:04 +0000

    2022/10/01 16:45:04 UTC  auto commit

--- custom-scripts/reboot.sh
+++ custom-scripts/reboot.sh
@@ -3,12 +3,18 @@
 cpuload=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
 ramload=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
 
+rebootdate() {
+  date +"%Y/%m/%d %H:%M:%S"
+}
+
 /var/www/private-sripts/telegram-notf.sh "
-REBOOT
+#REBOOT $(rebootdate)
 ---------------system------------
 build time ${buildtime}
 cpu load: ${cpuload}
 ram load: ${ramload}
+
+Bless! 🩴
 "
 
 reboot
