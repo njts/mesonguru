4cf1e2788403107b23cc18f57db32399fb1fb97e
njts <mayilov4+github.com@gmail.com>
2022-10-01 17:05:05 +0000

    2022/10/01 17:05:05 UTC  auto commit

--- custom-scripts/auto-git.sh
+++ custom-scripts/auto-git.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 start=`date +%s.%N`
+status=$(git status -s)
 cpuload=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
 ramload=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
-status=$(git status -s)
 
 # Build static website
 cd /var/www/html
