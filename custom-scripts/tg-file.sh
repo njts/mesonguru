1995dcf3a4264538a46f0cecdb8fc0da6c14a93c
njts <mayilov4+github.com@gmail.com>
2022-10-06 16:10:06 +0000

    2022/10/06 16:10:06 UTC  auto commit

--- custom-scripts/tg-file.sh
+++ custom-scripts/tg-file.sh
@@ -7,4 +7,6 @@ echo "/var/www/static/responsetime.csv" > "/var/www/static/responsetime_$(date "
 /var/www/private-sripts/telegram-notf.sh "@nijatoes here is your f*cking log files for $(date -I)"    
 curl -F document=@"/var/www/static/logs_$(date "+%Y.%m.%d").csv" https://api.telegram.org/bot$BOT_TOKEN/sendDocument?chat_id=$GROUP_ID
 curl -F document=@"/var/www/static/responsetime_$(date "+%Y.%m.%d").csv" https://api.telegram.org/bot$BOT_TOKEN/sendDocument?chat_id=$GROUP>
-rm /var/www/static/logs_$(date "+%Y.%m.%d").csv /var/www/static/responsetime_$(date "+%Y.%m.%d").csv
+echo "/var/www/static/logs.csv" >> "/var/www/static/archive-logs.csv"
+echo "/var/www/static/responsetime.csv" >> "/var/www/static/archive-responsetime.csv"
+rm /var/www/static/logs_$(date "+%Y.%m.%d").csv /var/www/static/responsetime_$(date "+%Y.%m.%d").csv /var/www/static/logs.csv /var/www/static/responsetime.csv
