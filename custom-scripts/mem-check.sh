ef899ffe072614621fd5c4bc3cf19931c04b6995
njts <mayilov4+github.com@gmail.com>
2022-10-06 07:50:04 +0000

    2022/10/06 07:50:03 UTC  auto commit

---
+++ custom-scripts/mem-check.sh
@@ -0,0 +1,8 @@
+# Reboot if memeory is lower than memory limit
+mem=$(cat /proc/meminfo | egrep "^MemFree" |awk '{print $2}')
+if (( mem <= $memlimit )); then
+/var/www/private-sripts/telegram-notf.sh "Free memory is $(($mem/1024)) MB and it's lower than $(($memlimit/1024)) MB limit, so we restart >
+/var/www/static/custom-scripts/reboot.sh
+else
+/var/www/private-sripts/telegram-notf.sh "Memory is fine. $(($mem/1024)) MB is free"
+fi
