dcc6713a1f8e82444602b25c3ffa8823d24030a0
njts <mayilov4+github.com@gmail.com>
2022-10-06 06:30:05 +0000

    2022/10/06 06:30:05 UTC  auto commit

--- custom-scripts/cron.sh
+++ custom-scripts/cron.sh
@@ -2,4 +2,4 @@
 /usr/bin/tmux new-session -d -s ENTER
 /usr/bin/tmux detach -s ENTER
 sleep 3
-/usr/bin/tmux send-keys -t 0 "/var/www/static/custom-scripts/auto-git.sh" Enter
+/usr/bin/tmux send-keys -t 0 "/var/www/static/custom-scripts/telegram-notf.sh REBOOTED!" Enter
