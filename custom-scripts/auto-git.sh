7325e6e40f11311a21bdf004237a870ae2d35d6e
njts <mayilov4+github.com@gmail.com>
2022-10-06 12:25:48 +0000

    2022/10/06 12:25:48 UTC  auto commit

--- custom-scripts/auto-git.sh
+++ custom-scripts/auto-git.sh
@@ -41,5 +41,7 @@ ${status}
 /var/www/static/custom-scripts/mem-check.sh
 responsetime=$(curl -H 'Cache-Control: no-cache, no-store' -s -w '\nLookup time:\t%{time_namelookup}\nConnect time:\t%{time_connect}\nPreXfer time:\t%{time_pretransfer}\nStartXfer time:\t%{time_starttransfer}\n\nTotal time:\t%{time_total}\n' -o /dev/null "https://meson.guru/?$(date +%s)")
 results="$(builddate) ${responsetime}"
-sed -e 's/\s\+/,/g' ${results} > responstime.xls
+echo ${results} > responstime.temp
+sed -e 's/\s\+/,/g' response > responstime.cvs
+rm responstime.temp
 /var/www/private-sripts/telegram-notf.sh "${results}"
