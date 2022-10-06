c946330c58cda7e78099cdff86e7e0cc9856873f
njts <mayilov4+github.com@gmail.com>
2022-10-06 10:05:07 +0000

    2022/10/06 10:05:07 UTC  auto commit

--- custom-scripts/up.sh
+++ custom-scripts/up.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
         
-        FILE=errors.txt
-        TARGET=meson.guru
-
-          touch $FILE
-          while true;
-          do
-if curl -s --head  --request GET https://example.com | grep "200 OK" > /dev/null; then 
+if curl -s --head  --request GET https://meson.guru | grep "200 OK" > /dev/null; then 
    echo "mysite.com is UP"
 else
    /var/www/private-sripts/telegram-notf.sh "@nijatoes f*cking server is down"
-sleep 5
-  fi
+fi
