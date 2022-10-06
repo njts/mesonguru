6ee4a746a598daa2fb14713e16227c53e271d7c1
njts <mayilov4+github.com@gmail.com>
2022-10-06 06:19:46 +0000

    2022/10/06 06:19:46 UTC  auto commit

--- custom-scripts/cron.sh
+++ custom-scripts/cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/usr/bin/tmux new-session -s -d ENTER
+/usr/bin/tmux new-session -d -s ENTER
 /usr/bin/tmux detach -s ENTER
 sleep 3
-tmux send-keys -t 0 "/var/www/static/custom-scripts/auto-git.sh" Enter
+/usr/bin/tmux send-keys -t 0 "/var/www/static/custom-scripts/auto-git.sh" Enter
