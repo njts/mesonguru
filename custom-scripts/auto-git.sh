5e13801755720451e89a9679b485023d2d37aa25
njts <mayilov4+github.com@gmail.com>
2022-10-06 12:08:42 +0000

    2022/10/06 12:08:42 UTC  auto commit

--- custom-scripts/auto-git.sh
+++ custom-scripts/auto-git.sh
@@ -40,4 +40,6 @@ ${status}
 "
 /var/www/static/custom-scripts/mem-check.sh
 responsetime=$(curl -H 'Cache-Control: no-cache, no-store' -s -w '\nLookup time:\t%{time_namelookup}\nConnect time:\t%{time_connect}\nPreXfer time:\t%{time_pretransfer}\nStartXfer time:\t%{time_starttransfer}\n\nTotal time:\t%{time_total}\n' -o /dev/null "https://meson.guru/?$(date +%s)")
+echo ${responsetime} >> responstime.txt
+sed -e 's/\s\+/,/g' responstime.txt > responstime.xls
 /var/www/private-sripts/telegram-notf.sh "${responsetime}"
--- custom-scripts/up.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-        
-if curl -s --head  --request GET https://meson.guru | grep "200 OK" > /dev/null; then 
-   echo "mysite.com is UP"
-else
-   /var/www/private-sripts/telegram-notf.sh "@nijatoes f*cking server is down"
-fi
