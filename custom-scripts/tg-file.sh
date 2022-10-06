7726daf4307343a1a55766306a9a1fdef03e3dc7
njts <mayilov4+github.com@gmail.com>
2022-10-06 14:25:07 +0000

    2022/10/06 14:25:07 UTC  auto commit

--- custom-scripts/tg-file.sh
+++ custom-scripts/tg-file.sh
@@ -2,6 +2,9 @@
 GROUP_ID=<group_id>
 BOT_TOKEN=<bot_token>
 
-/var/www/private-sripts/telegram-notf.sh "@nijatoes here is your f*cking log files"    
-curl -F document=@"/var/www/static/logs.csv" https://api.telegram.org/bot$BOT_TOKEN/sendDocument?chat_id=$GROUP_ID 
-curl -F document=@"/var/www/static/responsetime.csv" https://api.telegram.org/bot$BOT_TOKEN/sendDocument?chat_id=$GROUP_ID
+echo "/var/www/static/logs.csv" > "/var/www/static/logs_$(date "+%Y.%m.%d").csv"
+echo "/var/www/static/responsetime.csv" > "/var/www/static/responsetime_$(date "+%Y.%m.%d").csv"
+/var/www/private-sripts/telegram-notf.sh "@nijatoes here is your f*cking log files for $(date -I)"    
+curl -F document=@"/var/www/static/logs_$(date "+%Y.%m.%d").csv" https://api.telegram.org/bot$BOT_TOKEN/sendDocument?chat_id=$GROUP_ID
+curl -F document=@"/var/www/static/responsetime_$(date "+%Y.%m.%d").csv" https://api.telegram.org/bot$BOT_TOKEN/sendDocument?chat_id=$GROUP>
+rm /var/www/static/logs_$(date "+%Y.%m.%d").csv /var/www/static/responsetime_$(date "+%Y.%m.%d").csv
